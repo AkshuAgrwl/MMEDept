eb09cd90f0b19a6ac2d042d399d26852bb404078
Akshit Agarwal <139654279+AkshuAgrwl@users.noreply.github.com>
2026-01-03 01:35:27 +0530

    Initial setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import { ThemeProvider } from "@/components/theme-provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "MME LNMIIT",
+  description:
+    "Department of Mechanical-Mechatronics Engineering, LNMIIT Jaipur",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="h-60">
+          </div>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
