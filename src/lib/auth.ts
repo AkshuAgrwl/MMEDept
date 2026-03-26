bf2f935028d169cb037c451ba9da7daf39d91704
Akshit Agarwal <139654279+AkshuAgrwl@users.noreply.github.com>
2026-03-26 18:41:07 +0530

    Enable ghost user linking

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,32 @@
+import type { Adapter } from "@auth/core/adapters";
+
 import NextAuth from "next-auth";
 import { prisma } from "@/lib/prismarc";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { stripUndefined } from "@/lib/utils";
 
 import Google from "next-auth/providers/google";
 
+const baseAdapter = PrismaAdapter(prisma);
+const adapter: Adapter = {
+  ...baseAdapter,
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async createUser({ id, ...data }) {
+    const { email, ...updateData } = data;
+
+    return prisma.user.upsert({
+      where: { email: email },
+      update: stripUndefined(updateData),
+      create: stripUndefined(data),
+    });
+  },
+};
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   // debug: process.env.ENVIRONMENT === "development",
-  adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  adapter: adapter,
+  providers: [Google({ allowDangerousEmailAccountLinking: true })],
   session: { strategy: "jwt" },
   callbacks: {
     jwt({ token, user, account }) {
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function stripUndefined<T>(obj: T) {
+  const data = {} as T;
+  for (const key in obj) if (obj[key] !== undefined) data[key] = obj[key];
+  return data;
 }
